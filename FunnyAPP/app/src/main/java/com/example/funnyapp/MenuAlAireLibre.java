c3c39655c97a2c26bd2fbabdc1ffbc96685c1caf
isabel <isa.montero92@gmail.com>
2019-05-22 01:00:45 +0200

    aire libre creado

--- FunnyAPP/app/src/main/java/com/example/funnyapp/MenuAlAireLibre.java
+++ FunnyAPP/app/src/main/java/com/example/funnyapp/MenuAlAireLibre.java
@@ -2,12 +2,20 @@ package com.example.funnyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MenuAlAireLibre extends AppCompatActivity {
+
+    public Button but_BuscarCerca;
+    public Button but_Aleatorio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_al_aire_libre);
+        but_BuscarCerca = (Button) findViewById(R.id.but_BuscarCerca);
+        but_Aleatorio = (Button) findViewById(R.id.but_Aleatorio);
+
     }
 }
